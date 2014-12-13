1e20321015c1394d8758c7b9813281809e1732ce
loire <etienneloire@gmail.com>
2014-12-13 16:23:16 +0100

    second  commit

--- mpileup2phylip.py
+++ mpileup2phylip.py
@@ -16,10 +16,45 @@
 #_._._._._._._._._._._._._._._._._._._._._.*/
 #
 import sys
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 from Bio import SeqIO
-from Bio import SeqRecord
-from SeqRecord import SeqRecord
-from Bio import Seq
-from Seq import Seq
+
+infile = open("test.mpileup",'r')
+first = infile.readline().split()
+popname = first[1:]
+seq = [""]*len(popname)
+sec = infile.readline().split()
+prsc = sec[0]
+for i in range(len(sec[1:])):
+    seq[i]+=sec[i+1]
+
+for line in infile:
+    c = line.split()
+    sc= c[0]
+    if sc!=prsc:
+        handle = []
+        for i in xrange(len(popname)):
+            handle.append(SeqRecord(Seq(seq[i]),id = popname[i]))
+        SeqIO.write(handle, open(prsc+".phylip",'w'), "phylip")
+        seq = [""]*len(popname)
+        prsc=sc
+        for i in range(len(c[1:])):
+            seq[i]+=c[i+1]
+    else:
+        for i in range(len(c[1:])):
+            seq[i]+=c[i+1]
+        prsc=sc
+
+handle = []
+for i in xrange(len(popname)):
+    handle.append(SeqRecord(Seq(seq[i]),id = popname[i]))
+
+SeqIO.write(handle, open(sc+".phylip",'w'), "phylip")
+
+
+
+
+
 
 
