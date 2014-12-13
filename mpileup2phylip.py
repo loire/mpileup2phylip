f72914764d62f056ad882094351e43de5d5470ce
loire <etienneloire@gmail.com>
2014-12-13 16:18:49 +0100

    first commit

---
+++ mpileup2phylip.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+#
+#/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+#
+#* File Name : mpileup2phylip.py
+#
+#* Purpose :
+#
+#* Creation Date : 13-12-2014
+#
+#* Last Modified :
+#
+#* Created By :
+#
+#_._._._._._._._._._._._._._._._._._._._._.*/
+#
+import sys
+from Bio import SeqIO
+from Bio import SeqRecord
+from SeqRecord import SeqRecord
+from Bio import Seq
+from Seq import Seq
+
+
